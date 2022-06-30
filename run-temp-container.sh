d320ffcf1fa19f5f4d4b01bc6879f6fb628e8c3f
Bertram Truong <b@bertramtruong.com>
2022-07-01 02:47:45 +1000

    new command to run container

--- run-temp-container.sh
+++ run-temp-container.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
-docker run --rm -it -p 5901:5901 -p 6901:6901 --security-opt seccomp=./src/seccomp/chrome.json --shm-size=2g ubuntu-vnc
+#docker run --rm -it -p 5901:5901 -p 6901:6901 --security-opt seccomp=./src/seccomp/chrome.json --shm-size=2g ubuntu-vnc
+
+docker run --rm -it -p 5901:5901 -p 6901:6901 --privileged --shm-size=2g ubuntu-vnc
