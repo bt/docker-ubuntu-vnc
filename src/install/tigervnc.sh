fb95dce92121c7d6a4035d9ba621fad6180da7c0
Bertram Truong <b@bertramtruong.com>
2022-06-30 23:59:11 +1000

    fix wget and dependencies

--- src/install/tigervnc.sh
+++ src/install/tigervnc.sh
@@ -9,6 +9,7 @@ else
     URL="https://sourceforge.net/projects/tigervnc/files/stable/1.12.0/ubuntu-20.04LTS/amd64/tigervncserver_1.12.0-1ubuntu1_amd64.deb/download"
 fi
 
+apt-get install libxfont-dev libglu1-mesa x11-xkb-utils xfonts-base -y
 wget -O tigervnc_server.deb $URL
 dpkg -i tigervnc_server.deb
 rm tigervnc_server.deb
