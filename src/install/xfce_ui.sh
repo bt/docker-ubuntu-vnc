15cd022d3dcdf9e018af4d8c902f7948bf652dc3
Bertram Truong <b@bertramtruong.com>
2022-07-01 11:08:59 +1000

    disable screensaver as part of build

--- src/install/xfce_ui.sh
+++ src/install/xfce_ui.sh
@@ -7,3 +7,7 @@ apt-get update
 apt-get install -y xfce4 xfce4-goodies xfce4-terminal xterm xfce4-clipman-plugin
 apt-get purge -y pm-utils xscreensaver*
 apt-get clean -y
+
+echo "Disable screensaver"
+rm -rf /etc/xdg/autostart/xscreensaver.desktop
+rm -rf /etc/xdg/autostart/xfce4-screensaver.desktop
